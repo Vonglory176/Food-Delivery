a32ea7db9e02c7c14474c52655633cd14a266f27
Vonglory176 <SkylerGConley@gmail.com>
2024-09-21 18:56:51 -0700

    New header files

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-content'>
+        <h2>Order your favorite food here</h2>
+        <p>Choose from a diverse menu featuring a delectable range of dishes crafted with the finest ingredients and culinary expertise. We offer a delightful mix of savory classics and exotic flavors, ensuring there's something for every palate.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
