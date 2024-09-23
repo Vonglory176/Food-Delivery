d2933d267c4a1ebda94b7abe1137933b2ad351c1
Vonglory176 <SkylerGConley@gmail.com>
2024-09-22 18:38:14 -0700

    General update to links with addition of Cart / Login routing

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,27 +1,40 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
-const Navbar = () => {
-    const [menu, setMenu] = useState("Home")
+const Navbar: React.FC<NavbarProps> = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState<string>("Home")
+  const { cartHasItems } = useContext(StoreContext) // getTotalCartAmount
+
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="logo" />
+      <Link to={"/"}><img src={assets.logo} alt="logo" /></Link>
       <ul className="navbar-menu">
-        <li onClick={() => setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</li>
-        <li onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</li>
-        <li onClick={() => setMenu("Mobile-App")} className={menu === "Mobile-App" ? "active" : ""}>Mobile-App</li>
-        <li onClick={() => setMenu("Contact Us")} className={menu === "Contact Us" ? "active" : ""}>Contact Us</li>
+        <li>
+          <Link to={"/"} onClick={() => setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</Link>
+        </li>
+        <li>
+          <a href="#explore-menu" onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</a>
+        </li>
+        <li>
+          <a href="#app-download" onClick={() => setMenu("Mobile-App")} className={menu === "Mobile-App" ? "active" : ""}>Mobile-App</a>
+        </li>
+        <li>
+          <a href="#footer" onClick={() => setMenu("Contact Us")} className={menu === "Contact Us" ? "active" : ""}>Contact Us</a>
+        </li>
       </ul>
 
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="search" />
+        {/* <img src={assets.search_icon} alt="search" /> */}
 
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="basket" />
-          <div className="dot"></div>
+          <Link to={"/cart"}><img src={assets.basket_icon} alt="basket" /></Link>
+          <div className={cartHasItems ? "dot" : ""}></div>
+          {/* <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div> */}
         </div>
 
-        <button>Sign In</button>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
 
       </div>
 
