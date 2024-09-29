1c9814743870ffc4c0d41587b838e14fdf55cbc4
Vonglory176 <SkylerGConley@gmail.com>
2024-09-28 18:03:40 -0700

    Fixed some file pathing issues

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import authMiddleware from '../middleware/authMiddleware'
-import { placeOrder, verifyOrder, userOrders, adminOrders, adminUpdateOrderStatus } from '../controllers/orderController'
+import { authMiddleware } from '../middleware/auth.js'
+import { placeOrder, verifyOrder, userOrders, adminOrders, adminUpdateOrderStatus } from '../controllers/orderController.js'
 
 const orderRouter = express.Router()
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { loginUser, registerUser } from '../controllers/userController'
+// import { loginUser, registerUser } from '../controllers/test.js'
+import { loginUser, registerUser } from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
