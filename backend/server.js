2c31862636a702206c14134cbc305efa1d466acf
Vonglory176 <SkylerGConley@gmail.com>
2024-09-28 18:04:37 -0700

    Added CORS settings + cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import cookieParser from "cookie-parser"
 
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
@@ -19,8 +20,15 @@ const port = 4000
 
 
 // Middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json({ limit: '50mb' }))
+app.use(cors({
+    origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Ensure all methods needed are allowed
+    allowedHeaders: ['Content-Type', 'Authorization'], // Ensure headers your frontend sends are allowed
+    exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'], // Expose headers
+    credentials: true // Allows cookies
+}))
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 
 
@@ -30,8 +38,8 @@ connectDB()
 
 
 // API Endpoints
-app.use("/api/food", foodRouter)
 app.use("/images", express.static("uploads")) // path.join(__dirname, "uploads")
+app.use("/api/food", foodRouter)
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
 app.use("/api/order", orderRouter)
