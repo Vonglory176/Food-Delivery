b03e8b1aeff6f055e227ec74ca7ba856e5e42e65
Vonglory176 <SkylerGConley@gmail.com>
2024-09-28 17:56:59 -0700

    Added "axios.defaults.withCredentials = true"

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import axios from 'axios'
+
+// Set withCredentials globally
+axios.defaults.withCredentials = true
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
