c63542c24248689c197afaa043e2c854445dd776
Vonglory176 <SkylerGConley@gmail.com>
2024-09-28 17:57:48 -0700

    Changed <div> to <button>

--- frontend/src/components/exploreMenu/ExploreMenu.tsx
+++ frontend/src/components/exploreMenu/ExploreMenu.tsx
@@ -4,18 +4,22 @@ import { menu_list } from '../../assets/assets'
 const ExploreMenu: React.FC<ExploreMenuProps> = ({ category, setCategory }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
+
         <h1>Explore our menu</h1>
         <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable range of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+        
         <div className="explore-menu-list">
+
             {menu_list.map((item, index) => {
                 return (
-                <div onClick={() => setCategory(prev => prev === item.menu_name ? 'All' : item.menu_name)} key={index} className={`explore-menu-list-item`}> {/*  ${category === item.menu_name ? 'active' : ''} */}
+                <button onClick={() => setCategory(prev => prev === item.menu_name ? 'All' : item.menu_name)} key={index} className={`explore-menu-list-item`} aria-label={item.menu_name}> {/*  ${category === item.menu_name ? 'active' : ''} */}
                     <img className={category === item.menu_name ? 'active' : ''} src={item.menu_image} alt={item.menu_name} />
                     {/* <h3>{item.name}</h3> */}
                     <p>{item.menu_name}</p>
-                </div>
+                </button>
                 )
             })}
+
         </div>
 
         <hr />
