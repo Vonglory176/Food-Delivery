3908bed6de186cfcbc92f8e35aa988d45ba2510b
Vonglory176 <SkylerGConley@gmail.com>
2024-09-28 17:58:47 -0700

    Added anchor wrappers to images

--- frontend/src/components/appDownload/AppDownload.tsx
+++ frontend/src/components/appDownload/AppDownload.tsx
@@ -6,8 +6,8 @@ const AppDownload = () => {
     <div className='app-download' id='app-download'>
         <p>For Better Experience Download <br /> Our App</p>
         <div className="app-download-platforms">
-            <img src={assets.play_store} alt="play-store" />
-            <img src={assets.app_store} alt="app-store" />
+            <a href="https://play.google.com" target='_blank' aria-label='Play Store'><img src={assets.play_store} alt="play-store" /></a>
+            <a href="https://apps.apple.com" target='_blank' aria-label='App Store'><img src={assets.app_store} alt="app-store" /></a>
         </div>
     </div>
   )
