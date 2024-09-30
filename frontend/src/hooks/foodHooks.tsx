627a616221ac5f473980d8dda7f623734e22f1cb
Vonglory176 <SkylerGConley@gmail.com>
2024-09-29 19:54:22 -0700

    Added whitespace

--- frontend/src/hooks/foodHooks.tsx
+++ frontend/src/hooks/foodHooks.tsx
@@ -6,7 +6,9 @@ import axios from "axios"
 export const getFoodHook = async (setFoodItems: React.Dispatch<React.SetStateAction<any>>) => {
     try {
         const response = await axios.get(import.meta.env.VITE_BACKEND_URL + '/api/food/list')
+
         setFoodItems(response.data.foodItems || [])
+        
     } catch (error) {
         console.error("ERROR: Could not get food items")
     }
