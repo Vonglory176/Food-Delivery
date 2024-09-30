54535a7d3343355dfcb25040dacecad0e7df2794
Vonglory176 <SkylerGConley@gmail.com>
2024-09-29 19:47:36 -0700

    Added routes for 'logout' & token generation

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
 // import { loginUser, registerUser } from '../controllers/test.js'
-import { loginUser, registerUser } from '../controllers/userController.js'
+import { loginUser, registerUser, logoutUser, generateAccessToken } from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/login', loginUser)
 userRouter.post('/register', registerUser)
+userRouter.post('/logout', logoutUser)
+userRouter.get('/generate-access-token', generateAccessToken)
 
 export default userRouter
