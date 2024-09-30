b8da4662089ffb2434c89e093763f0bd44d3c118
Vonglory176 <SkylerGConley@gmail.com>
2024-09-29 19:55:51 -0700

    General tweaks and cleanup, non-fetch logic now in StoreContext

--- frontend/src/hooks/userHooks.tsx
+++ frontend/src/hooks/userHooks.tsx
@@ -3,39 +3,33 @@ import axios from "axios"
 // USER HOOKS ////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Login / Sign-Up (Covers both)
-export const loginHook = async (data: any, action: 'register' | 'login', updateAuthState, setShowLogin, setErrors) => { // setToken: React.Dispatch<React.SetStateAction<string | null>>
+export const loginSignupHook = async (data, action: string) => { // , updateAuthState, setShowLogin
 
     try {
         const response = await axios.post(import.meta.env.VITE_BACKEND_URL + '/api/user/' + action, data)
         console.log(response)
 
-        if (response.data.success) {
-            // updateAuthState({accessToken: response.data.accessToken, user: response.data.user})
-            setShowLogin(false)
-        }
+        return response
     }
     catch (error: any) {
         console.error(error)
-        // console.error("ERROR: Could not login")
-
-        const {type="email", message="Internal Server Error"} = error?.response?.data?.error
-        setErrors(prev => ({...prev, [type]: message}))
+        return error.response
     }
 }
 
 // Logout
-export const logoutHook = async (updateAuthState) => {
+export const logoutHook = async () => { // updateAuthState
     try {
         const response = await axios.post(import.meta.env.VITE_BACKEND_URL + '/api/user/logout')
         console.log(response)
 
         // Optionally check response status
         if (response.status !== 200) throw new Error("Server-side logout failed: " + response.status)
+
+        return response
     }
     catch (error) {
         console.error(error)
-        console.error("ERROR: Could not logout")
+        return error.response
     }
-
-    updateAuthState({accessToken: null, user: null})
 }
