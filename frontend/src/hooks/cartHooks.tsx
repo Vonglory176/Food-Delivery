eccb479578653f4c67ab63b4ac05ad2abb2c6c9c
Vonglory176 <SkylerGConley@gmail.com>
2024-10-03 16:15:31 -0700

    General cleanup + 'setCartItems({})' on failure to sync

--- frontend/src/hooks/cartHooks.tsx
+++ frontend/src/hooks/cartHooks.tsx
@@ -3,27 +3,27 @@
 // CART HOOKS ////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Sync Cart (On login / checkout)
-export const syncCartHook = async (authCustomFetch, cartItems={}, setCartItems: React.Dispatch<React.SetStateAction<any>>, setCartLoading) => {
-    console.log(cartItems)
+export const syncCartHook = async (authCustomFetch, cartItems={}, setCartItems: React.Dispatch<React.SetStateAction<any>>) => { // , setCartLoading = () => {}
+    // console.log(cartItems)
 
     try {
         const response = await authCustomFetch(import.meta.env.VITE_BACKEND_URL + '/api/cart/sync', {
             method: 'POST',
             headers: {
-                // 'Method': 'GET',
-                // 'Authorization': `Bearer ${token}`
                 'Content-Type': 'application/json',
             },
-            data: { // Change made here
-                cartData: cartItems === cartItems // Determinded by login state
+            data: {
+                cartData: cartItems // Determinded by login state
             }
         })
 
-        setCartItems(response.data.cartData)
-        setCartLoading(false)
+        setCartItems(response.data.cartData || {})
+        // setCartLoading(false)
 
     } catch (error) {
+        // console.log(error)
         console.error("ERROR: Could not load cart data")
+        setCartItems({})
     }
 }
 
@@ -34,11 +34,7 @@ export const updateCartHook = async (authCustomFetch, itemId: string, action: 'a
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
-                // 'Authorization': `Bearer ${token}`
-            },
-            // body: {
-            //     itemId
-            // }
+            }
         })
 
         if (!response.data.success) throw new Error
