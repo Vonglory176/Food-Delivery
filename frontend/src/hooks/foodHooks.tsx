acd0df3e5cfcd623719f1caa1493496f9ddf4c47
Vonglory176 <SkylerGConley@gmail.com>
2024-10-02 18:45:14 -0700

    Fixed typo with return variable

--- frontend/src/hooks/foodHooks.tsx
+++ frontend/src/hooks/foodHooks.tsx
@@ -6,8 +6,9 @@ import axios from "axios"
 export const getFoodHook = async (setFoodItems: React.Dispatch<React.SetStateAction<any>>) => {
     try {
         const response = await axios.get(import.meta.env.VITE_BACKEND_URL + '/api/food/list')
+        console.log(response)
 
-        setFoodItems(response.data.foodItems || [])
+        setFoodItems(response.data.foodList || [])
         
     } catch (error) {
         console.error("ERROR: Could not get food items")
