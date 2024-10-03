cfaf71eff489797351e2d817dd8eeb0cd6c04bff
Vonglory176 <SkylerGConley@gmail.com>
2024-10-03 16:08:16 -0700

    Removed comment code + updated env var

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,29 +1,5 @@
 import jwt from 'jsonwebtoken'
 
-// export const authMiddleware = async (req, res, next) => {
-//     console.log("\n IN AUTH MIDDLEWARE --------------------------- \n")
-//     const { token } = req.headers // req.cookies
-
-//     // const authHeader = req.headers.authorization
-//     // const token = authHeader && authHeader.split(' ')[1]
-
-//     try {
-//         // If no Access Token
-//         if(!token) throw { status: 401, message: 'User is not authorized' }
-        
-//         const token_decoded = jwt.verify(token, process.env.JWT_SECRET)
-//         req.body.userId = token_decoded.id
-        
-//         next()
-//     } catch (error) {
-
-//         console.log("ERROR IN AUTH MIDDLEWARE")
-//         console.error(error)
-
-//         next(error)
-//     }
-// }
-
 // This is for fetch calls that always need the user to be logged-in
 export const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization
@@ -36,7 +12,7 @@ export const authMiddleware = (req, res, next) => {
         // If no Access Token
         if(!token) throw { status: 401, message: 'User is not authorized' }
 
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        jwt.verify(token, process.env.FRONTEND_ACCESS_TOKEN_SECRET, (err, decoded) => {
             // if (err) console.log(err)
             if (err) throw { status: 403, message: 'Verification failed' }
 
