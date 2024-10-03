952cdbabd7a5304dd146b786ae43f5037bb973eb
Vonglory176 <SkylerGConley@gmail.com>
2024-10-02 18:40:47 -0700

    Changed code formatting

--- frontend/src/components/appDownload/AppDownload.tsx
+++ frontend/src/components/appDownload/AppDownload.tsx
@@ -4,11 +4,11 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-        <p>For Better Experience Download <br /> Our App</p>
-        <div className="app-download-platforms">
-            <a href="https://play.google.com" target='_blank' aria-label='Play Store'><img src={assets.play_store} alt="play-store" /></a>
-            <a href="https://apps.apple.com" target='_blank' aria-label='App Store'><img src={assets.app_store} alt="app-store" /></a>
-        </div>
+      <p>For Better Experience Download <br /> Our App</p>
+      <div className="app-download-platforms">
+        <a href="https://play.google.com" target='_blank' aria-label='Play Store'><img src={assets.play_store} alt="play-store" /></a>
+        <a href="https://apps.apple.com" target='_blank' aria-label='App Store'><img src={assets.app_store} alt="app-store" /></a>
+      </div>
     </div>
   )
 }
