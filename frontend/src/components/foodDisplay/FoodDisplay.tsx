ceb37023d0054ca794916b283deb18e9debd4ac4
Vonglory176 <SkylerGConley@gmail.com>
2024-10-02 18:41:54 -0700

    Fixed typos & added code to include spinner while loading

--- frontend/src/components/foodDisplay/FoodDisplay.tsx
+++ frontend/src/components/foodDisplay/FoodDisplay.tsx
@@ -1,22 +1,39 @@
-import React, { useContext } from 'react'
-import { StoreContext, useStore } from '../../context/StoreContext'
+import React from 'react'
+import { useStore } from '../../context/StoreContext'
 import FoodItem from '../foodItem/FoodItem'
 
 const FoodDisplay: React.FC<FoodDisplayProps> = ({ category }) => {
-    const { food_list } = useStore()
+    const { foodList } = useStore()
 
     return (
         <div className='food-display' id='food-display'>
             <h2>Top dishes near you</h2>
             <div className="food-display-list">
-                {food_list.map((item: any, index: number) => {
+                {foodList.length === 0 ? 
+                
+                    // If no food items
+                    <div className="spinner center"></div> 
 
-                    if (category === "All" || category === item.category) {
-                        return <FoodItem key={index} id={item._id} name={item.name} image={item.image} description={item.description} price={item.price} />
-                    }
+                    :
+
+                    // If no items in category
+                    foodList.filter((item: any) => category === "All" || item.category === category).length === 0 ? (
+                        <p className='center'>No foods found in {category}.</p>
+                    )
                     
-                })}
+                    :
+
+                    // If items in category
+                    foodList.map((item: any, index: number) => {
+
+                        if (category === "All" || category === item.category) {
+                            return <FoodItem key={index} id={item._id} name={item.name} image={item.image} description={item.description} price={item.price} />
+                        }
+
+                    })}
             </div>
+
+            <hr />
         </div>
     )
 }
