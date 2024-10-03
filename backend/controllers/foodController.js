1daeee4722b155c43494cb0790ef800da030ba15
Vonglory176 <SkylerGConley@gmail.com>
2024-10-02 18:37:53 -0700

    Changes to get 'removeFood' working

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -6,6 +6,7 @@ const createFood = async (req, res, next) => {
     console.log("\n IN CREATE FOOD --------------------------- \n")
 
     const requiredFields = ["name", "description", "price", "category"] // Not including image    
+    console.log(req.body)
 
     try {
         if (!req.file || !requiredFields.every(field => field in req.body)) {
@@ -13,7 +14,7 @@ const createFood = async (req, res, next) => {
         }
 
 
-        let image_filename = `${req.file.filename}`    
+        let image_filename = `${req.file.filename}`
         const food = new foodModel({
             name: req.body.name,
             description: req.body.description,
@@ -71,20 +72,31 @@ const getFood = async (req, res, next) => {
 // REMOVE FOOD
 const removeFood = async (req, res, next) => {
     console.log("\n IN REMOVE FOOD --------------------------- \n")
+    console.log(req.body, req.params, req.query)
 
+    
     try {
+        // Get ID & Food
+        const { itemId } = req.params
+        if (!itemId) throw { status: 400, clientMessage: 'Missing required fields' }
 
-        const foods = await foodModel.findById(req.body.id)
-        fs.unlink(`uploads/${foods.image}`, (err) => {
+        const foods = await foodModel.findById(itemId)
+        
+
+        // Remove Image
+        fs.unlink(`uploads/${foods?.image}`, (err) => {
             if (err) {
                 console.error(err)
                 return
             }
-            console.log(`${foods.image} was deleted`)
+            else console.log(`Image was deleted`)
         })
 
-        await foodModel.findByIdAndDelete(req.body.id)
+        // Remove Food
+        await foodModel.findByIdAndDelete(itemId)
 
+
+        // Send Resposne
         const successMessage = "Food removed successfully"
         res.status(200).json({
             success: true,
