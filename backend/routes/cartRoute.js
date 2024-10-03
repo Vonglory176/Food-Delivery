3ee3b3d571a79f3dacabca9ad83e41494061d127
Vonglory176 <SkylerGConley@gmail.com>
2024-10-02 18:39:53 -0700

    Updated route Methods & changed getCart to syncCart

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { addToCart, removeFromCart, getCart } from '../controllers/cartController.js'
+import { addToCart, removeFromCart, syncCart } from '../controllers/cartController.js'
 import { authMiddleware } from '../middleware/auth.js'
 
 const cartRouter = express.Router()
 
-cartRouter.post('/add/:id', authMiddleware, addToCart)
-cartRouter.post('/remove/:id', authMiddleware, removeFromCart)
-cartRouter.get('/get/:id', authMiddleware, getCart)
+cartRouter.patch('/add/:itemId', authMiddleware, addToCart)
+cartRouter.patch('/remove/:itemId', authMiddleware, removeFromCart)
+cartRouter.post('/sync', authMiddleware, syncCart)
 
 export default cartRouter
