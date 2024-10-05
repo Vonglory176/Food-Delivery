f7678fe42171a7a00c3daa8e156abe544a34d4ce
Vonglory176 <SkylerGConley@gmail.com>
2024-10-04 18:44:45 -0700

    New route file with auth paths

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { generateAccessToken, accountLogout, accountLogin, accountRegister } from '../controllers/common/authController.js'
+import { authMiddleware } from '../middleware/auth.js'
+
+const authRouter = express.Router()
+
+// User routes
+authRouter.post('/register', accountRegister)
+
+// Admin routes
+authRouter.post('/admin-register', authMiddleware, accountRegister)
+
+// Common routes
+authRouter.get('/generate-access-token', generateAccessToken)
+authRouter.post('/login', accountLogin)
+authRouter.post('/logout', accountLogout)
+
+export default authRouter
