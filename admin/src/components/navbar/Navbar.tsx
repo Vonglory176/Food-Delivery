ba08ddbbe73cca50e995b5a992345f7f89ac89f0
Vonglory176 <SkylerGConley@gmail.com>
2024-10-04 18:37:01 -0700

    Swapped profile icon w/logout button

--- admin/src/components/navbar/Navbar.tsx
+++ admin/src/components/navbar/Navbar.tsx
@@ -1,12 +1,25 @@
 // import React from 'react'
+import { Link } from 'react-router-dom'
 import { assets } from '../../assets/assets'
+import { useAdmin } from '../../context/adminContext'
 
 const Navbar = () => {
+  const { userLogout } = useAdmin()
+
   return (
+
     <div className='navbar'>
-      <img className='logo' src={assets.logo} alt="logo" />
-      <img className="profile" src={assets.profile_image} alt="profile" />
+
+      <Link to='/'><img className='logo' src={assets.logo} alt="logo" /></Link>
+      
+      {/* <img className="profile" src={assets.profile_image} alt="profile" /> */}
+      <button className='logout-btn' onClick={userLogout} aria-label='logout'>
+        {/* <img src={assets.logout_icon} alt="" />  */}
+        Logout
+      </button>
+
     </div>
+
   )
 }
 
