6bfcdb2174d2f3a2dc966945f1945709a5667abd
Vonglory176 <SkylerGConley@gmail.com>
2024-10-04 18:41:57 -0700

    Removed 'adminGetOrders' in favor of common use controller

--- backend/controllers/admin/orderController.js
+++ backend/controllers/admin/orderController.js
@@ -1,36 +1,3 @@
-
-
-// GET ORDERS (Admin)
-const adminGetOrders = async (req, res, next) => {
-    console.log("\n IN ADMIN ORDER LIST --------------------------- \n")
-
-    // const requiredFields = ["userId"]
-
-    try {
-        // if (!requiredFields.every(field => field in req.body)) {
-        //     throw { status: 400, clientMessage: 'Missing required fields' }
-        // }
-
-        const orders = await orderModel.find({})
-
-        res.status(200).json({
-            success: true,
-            data: orders
-        })
-        
-        console.log("Orders fetched successfully")
-
-    } catch (error) {
-
-        console.log("\n ERROR IN ADMIN ORDER LIST --------------------------- \n")
-        console.log(error)
-
-        error.action = "Fetching admin order list"
-        next(error)
-    }
-}
-
-
 // UPDATE ORDER STATUS (Admin)
 const adminUpdateOrderStatus = async (req, res, next) => {
     console.log("\n IN ADMIN UPDATE ORDER STATUS --------------------------- \n")
@@ -63,6 +30,35 @@ const adminUpdateOrderStatus = async (req, res, next) => {
 }
 
 export {
-    adminGetOrders,
     adminUpdateOrderStatus
 }
+
+// // GET ORDERS (Admin)
+// const adminGetOrders = async (req, res, next) => {
+//     console.log("\n IN ADMIN ORDER LIST --------------------------- \n")
+
+//     // const requiredFields = ["userId"]
+
+//     try {
+//         // if (!requiredFields.every(field => field in req.body)) {
+//         //     throw { status: 400, clientMessage: 'Missing required fields' }
+//         // }
+
+//         const orders = await orderModel.find({})
+
+//         res.status(200).json({
+//             success: true,
+//             data: orders
+//         })
+        
+//         console.log("Orders fetched successfully")
+
+//     } catch (error) {
+
+//         console.log("\n ERROR IN ADMIN ORDER LIST --------------------------- \n")
+//         console.log(error)
+
+//         error.action = "Fetching admin order list"
+//         next(error)
+//     }
+// }
