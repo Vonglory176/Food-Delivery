63f317ab7285660769e58ec1d4ebbec862a52dbe
Vonglory176 <SkylerGConley@gmail.com>
2024-10-04 18:40:57 -0700

    New food hooks file ripped from Front-End

---
+++ admin/src/hooks/foodHooks.tsx
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+// FOOD HOOKS ////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Get Food Items (Server)
+export const getFoodHook = async (setFoodItems: React.Dispatch<React.SetStateAction<any>>) => {
+    try {
+        const response = await axios.get(import.meta.env.VITE_BACKEND_URL + '/api/food/list', {
+            headers: {
+                'x-request-source': 'admin'
+            }
+        })
+        // console.log(response)
+
+        setFoodItems(response?.data?.foodList || [])
+        
+    } catch (error) {
+        setFoodItems({})
+        // console.log(error)
+        console.error("ERROR: Could not get food items")
+    }
+}
