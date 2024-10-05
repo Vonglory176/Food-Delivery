2a13c027fe10b315fccdbe65e9efa727959b9531
Vonglory176 <SkylerGConley@gmail.com>
2024-10-04 18:45:36 -0700

    Added authMiddleware to food actions

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,15 +1,15 @@
 import express from "express"
 import { adminCreateFood, adminRemoveFood } from "../controllers/admin/foodController.js"
 import { getFood } from "../controllers/common/foodController.js"
+import { authMiddleware } from "../middleware/auth.js"
 import upload from '../config/multerConfig.js'
-
 
 const foodRouter = express.Router()
 
 
 // Admin routes
-foodRouter.post("/add", upload.single("image"), adminCreateFood)
-foodRouter.delete("/remove/:id", adminRemoveFood)
+foodRouter.post("/add", authMiddleware, upload.single("image"), adminCreateFood)
+foodRouter.delete("/remove/:id", authMiddleware, adminRemoveFood)
 
 
 // Common routes
