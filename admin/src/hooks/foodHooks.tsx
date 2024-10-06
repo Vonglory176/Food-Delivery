6b832c40440480502774ebc05b2880d779023a5e
Vonglory176 <SkylerGConley@gmail.com>
2024-10-05 17:12:04 -0700

    Food hooks moved in from components working perfectly

--- admin/src/hooks/foodHooks.tsx
+++ admin/src/hooks/foodHooks.tsx
@@ -2,21 +2,54 @@ import axios from "axios"
 
 // FOOD HOOKS ////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Get Food Items (Server)
-export const getFoodHook = async (setFoodItems: React.Dispatch<React.SetStateAction<any>>) => {
+// Get Food Items
+export const getFoodHook = async () => {
     try {
-        const response = await axios.get(import.meta.env.VITE_BACKEND_URL + '/api/food/list', {
-            headers: {
-                'x-request-source': 'admin'
-            }
-        })
+        const response = await axios.get(import.meta.env.VITE_BACKEND_URL + '/api/food/list')
         // console.log(response)
 
-        setFoodItems(response?.data?.foodList || [])
+        return response
         
     } catch (error) {
-        setFoodItems({})
         // console.log(error)
+
         console.error("ERROR: Could not get food items")
+        return error.response
     }
 }
+
+// Add Food Item
+export const addFoodHook = async (authCustomFetch: any, formData: FormData) => {
+    console.log(formData)
+    try {
+        const response = await authCustomFetch(import.meta.env.VITE_BACKEND_URL + '/api/food/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            data: formData
+        })
+        return response
+
+    } catch (error) {
+
+        console.error("ERROR: Could not add food item")
+        return error.response
+
+    }
+}
+
+//  Remove Food Item
+export const removeFoodHook = async (authCustomFetch: any, foodId: string) => {
+    try {
+        const response = await authCustomFetch(import.meta.env.VITE_BACKEND_URL + '/api/food/remove/' + foodId, {
+            method: 'DELETE'
+        })
+        return response
+
+    } catch (error) {
+        console.error("ERROR: Could not remove food item")
+        return error.response
+    }
+}
+
