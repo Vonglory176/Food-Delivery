9dd9899ffdfb4c4acd1da00f2c70a851d74cd0ff
Vonglory176 <SkylerGConley@gmail.com>
2024-10-05 17:09:36 -0700

    Added new icons & changed min-max width values for sidebar

--- admin/src/components/sidebar/Sidebar.tsx
+++ admin/src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 // import React from 'react'
+import { FaList, FaUtensils, FaPlusCircle } from 'react-icons/fa'
 import { assets } from '../../assets/assets'
 import { NavLink } from 'react-router-dom'
 
@@ -7,19 +8,22 @@ const Sidebar = () => {
     <div className='sidebar'>
       <div className="sidebar-options">
 
-        <NavLink to="/add" className="sidebar-option">
-            <img src={assets.add_icon} alt="" />
-            <p>Add Items</p>
+        <NavLink to="/add" className="sidebar-option" title='Add Food' aria-label='Add Food'>
+          {/* <img src={assets.add_icon} alt="" /> */}
+          <FaPlusCircle size={24} />
+          <p>Add Food</p>
         </NavLink>
 
-        <NavLink to="/list" className="sidebar-option">
-            <img src={assets.order_icon} alt="" />
-            <p>List Items</p>
+        <NavLink to="/foods" className="sidebar-option" title='Food List' aria-label='Food List'>
+          {/* <img src={assets.order_icon} alt="" /> */}
+          <FaUtensils size={24} />
+          <p>Food List</p>
         </NavLink>
 
-        <NavLink to="/orders" className="sidebar-option">
-            <img src={assets.order_icon} alt="" />
-            <p>Orders</p>
+        <NavLink to="/orders" className="sidebar-option" title='Order List' aria-label='Order List'>
+          {/* <img src={assets.order_icon} alt="" /> */}
+          <FaList size={24} />
+          <p>Order List</p>
         </NavLink>
 
       </div>
