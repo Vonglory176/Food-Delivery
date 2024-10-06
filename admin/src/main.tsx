45618a8a3ae044892cec05a570c7c09722ff5191
Vonglory176 <SkylerGConley@gmail.com>
2024-10-05 17:07:14 -0700

    Added 'axios.defaults.headers.common['x-request-source'] = 'admin''

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -8,6 +8,7 @@ import AdminContextProvider from './context/adminContext.tsx'
 
 // Set withCredentials globally
 axios.defaults.withCredentials = true
+axios.defaults.headers.common['x-request-source'] = 'admin' // Tells the Back-End that requests are from the Admin-Panel
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
