f1b7b50a8a73cd547cad32bb9a7bbe7aa8a86da8
Vonglory176 <SkylerGConley@gmail.com>
2024-10-06 19:08:07 -0700

    Added 'demoMiddleware' to restrict admin features post production

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken'
+// import userModel from '../models/userModel.js'
+import adminModel from '../models/adminModel.js'
 
 // This is for fetch calls that always need the user to be logged-in
 export const authMiddleware = (req, res, next) => {
@@ -36,6 +38,35 @@ export const authMiddleware = (req, res, next) => {
         console.log("\n ERROR IN JWT AUTH MIDDLEWARE ---------------------------------- \n")
         // console.error(error)
 
+        next(error)
+    }
+}
+
+export const demoMiddleware = async (req, res, next) => {
+    console.log("\n IN DEMO-MODE MIDDLEWARE --------------------------- \n")
+    // console.log(req.body, req.params, req.query)
+
+    try {
+        // console.log(req.body)
+        const { userId, isAdmin } = req.body
+
+        if (isAdmin) {
+
+            const user = await adminModel.findById(userId)
+            console.log(user.demo)
+
+            if (!user) throw { status: 404, clientMessage: 'User not found' }
+            if (user.demo) throw { status: 403, clientMessage: 'User is in Demo-Mode and cannot perform this action' }
+        }
+
+
+
+        next()
+
+    } catch (error) {
+        console.log("\n ERROR IN DEMO-MODE MIDDLEWARE ---------------------------------- \n")
+        console.log(error)
+        error.action = "Checking if in Demo-Mode"
         next(error)
     }
 }
