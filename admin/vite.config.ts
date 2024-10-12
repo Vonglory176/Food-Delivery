33354742e77c944a9a37bb48180c9e7381908842
Vonglory176 <SkylerGConley@gmail.com>
2024-10-12 14:07:31 -0700

    Updated base paths for new URL

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/' // '/Food-Delivery/admin/'
+  base: '/' // '/Food-Delivery/admin/'
 })
